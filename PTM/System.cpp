d2f6db12a08c399b89d37ab8d0bf7701da192920
Fernando <fernando.oliveira@hydrasystems.com.br>
2022-01-05 16:32:34 -0300

    View history

--- PTM/System.cpp
+++ PTM/System.cpp
@@ -1,8 +1,6 @@
 #include "System.h"
 #include "Boot.h"
 
-#define OP(x)	Op[#x] = &x
-
 std::map<std::string, Variable> Vars;
 int KeyPressed = 0;
 int CmpResult = 0;
@@ -397,37 +395,37 @@ void JP()
 	Argc(1);
 	Jump(ArgLabel());
 }
-void JE()
+void JP_E()
 {
 	Argc(1);
 	if (CmpResult == 0)
 		Jump(ArgLabel());
 }
-void JNE()
+void JP_NE()
 {
 	Argc(1);
 	if (CmpResult != 0)
 		Jump(ArgLabel());
 }
-void JG()
+void JP_G()
 {
 	Argc(1);
 	if (CmpResult > 0)
 		Jump(ArgLabel());
 }
-void JGE()
+void JP_GE()
 {
 	Argc(1);
 	if (CmpResult >= 0)
 		Jump(ArgLabel());
 }
-void JL()
+void JP_L()
 {
 	Argc(1);
 	if (CmpResult < 0)
 		Jump(ArgLabel());
 }
-void JLE()
+void JP_LE()
 {
 	Argc(1);
 	if (CmpResult <= 0)
@@ -437,6 +435,42 @@ void CALL()
 {
 	Argc(1);
 	Call(ArgLabel());
+}
+void CALL_E()
+{
+	Argc(1);
+	if (CmpResult == 0)
+		Call(ArgLabel());
+}
+void CALL_NE()
+{
+	Argc(1);
+	if (CmpResult != 0)
+		Call(ArgLabel());
+}
+void CALL_G()
+{
+	Argc(1);
+	if (CmpResult > 0)
+		Call(ArgLabel());
+}
+void CALL_GE()
+{
+	Argc(1);
+	if (CmpResult >= 0)
+		Call(ArgLabel());
+}
+void CALL_L()
+{
+	Argc(1);
+	if (CmpResult < 0)
+		Call(ArgLabel());
+}
+void CALL_LE()
+{
+	Argc(1);
+	if (CmpResult <= 0)
+		Call(ArgLabel());
 }
 void RET()
 {
@@ -589,7 +623,7 @@ void PAUSE()
 	auto ms = ArgNumber();
 	SDL_Delay(ms);
 }
-void READ()
+void FREAD()
 {
 	Argc(2);
 	auto id = ArgVariableName(true);
@@ -599,14 +633,14 @@ void READ()
 	auto data = File::ReadText(path);
 	Vars[id].String = data;
 }
-void WRITE()
+void FWRITE()
 {
 	Argc(2);
 	auto data = ArgString();
 	auto filePath = ArgString();
 	File::WriteText(filePath, data);
 }
-void DEL()
+void FDEL()
 {
 	Argc(1);
 	auto path = ArgString();
@@ -616,74 +650,80 @@ void DEL()
 void InitCommands()
 {
 	//=== MISC ===
-	OP(NOP);	// No operation
+	Op["NOP"] = &NOP;			// No operation
 
 	//=== VARIABLES ===
-	OP(NUM);	// Declare variable as number
-	OP(STR);	// Declare variable as string
+	Op["NUM"] = &NUM;			// Declare variable as number
+	Op["STR"] = &STR;			// Declare variable as string
 	Op["NUM[]"] = &NUM_ARRAY;	// Declare variable as number array
 	Op["STR[]"] = &STR_ARRAY;	// Declare variable as string array
-	OP(SET);	// Set value to variable
-	OP(PUSH);	// Push value into array
-	OP(COUNT);	// Get number of items in array
-	OP(RND);	// Set random number value variable
+	Op["SET"] = &SET;			// Set value to variable
+	Op["PUSH"] = &PUSH;			// Push value into array
+	Op["COUNT"] = &COUNT;		// Get number of items in array
+	Op["RND"] = &RND;			// Set random number value variable
 
 	//=== PROGRAM FLOW ===
-	OP(EXIT);	// Exit program normally
-	OP(HALT);	// Stop program execution
-	OP(JP);		// Jump
-	OP(JE);		// Jump if equal
-	OP(JNE);	// Jump if not equal
-	OP(JG);		// Jump if greater
-	OP(JGE);	// Jump if greater or equal
-	OP(JL);		// Jump if less
-	OP(JLE);	// Jump if less or equal
-	OP(CALL);	// Call
-	OP(RET);	// Return
-	OP(PAUSE);	// Pause program execution
-
+	Op["EXIT"] = &EXIT;			// Exit program normally
+	Op["HALT"] = &HALT;			// Stop program execution
+	Op["JP"] = &JP;				// Jump
+	Op["JP=" ] = &JP_E;			// Jump if equal
+	Op["JP!="] = &JP_NE;		// Jump if not equal
+	Op["JP>" ] = &JP_G;			// Jump if greater
+	Op["JP>="] = &JP_GE;		// Jump if greater or equal
+	Op["JP<" ] = &JP_L;			// Jump if less
+	Op["JP<="] = &JP_LE;		// Jump if less or equal
+	Op["CALL"] = &CALL;			// Call
+	Op["CALL="] = &CALL_E;		// Call if equal
+	Op["CALL!="] = &CALL_NE;	// Call if not equal
+	Op["CALL>"] = &CALL_G;		// Call if greater
+	Op["CALL>="] = &CALL_GE;	// Call if greater or equal
+	Op["CALL<"] = &CALL_L;		// Call if less
+	Op["CALL<="] = &CALL_LE;	// Call if less or equal
+	Op["RET"] = &RET;			// Return
+	Op["PAUSE"] = &PAUSE;		// Pause program execution
+	
 	//=== MATH ===
-	OP(ADD);	// Add to number variable
+	Op["ADD"] = &ADD;			// Add to number variable
 	
 	//=== COMPARE ===
-	OP(CMP);	// Compare value with number variable
+	Op["CMP"] = &CMP;			// Compare value with number variable
 	
 	//=== DEBUG ===
-	OP(MSGB);	// Show message box
+	Op["MSGB"] = &MSGB;			// Show message box
 
 	//=== WINDOW ===
-	OP(WINDOW); // Open window
-	OP(TITLE);	// Set window title
+	Op["WINDOW"] = &WINDOW;		// Open window
+	Op["TITLE"] = &TITLE;		// Set window title
 
 	//=== GRAPHICS ===
-	OP(FWND);	// Enable/disable fullscreen mode
-	OP(UPDS);	// Enable/disable automatic screen update
-	OP(CLS);	// Clear screen
-	OP(OUTM);	// Select output mode
-	OP(OUT);	// Output tile to screen
-	OP(OUTS);	// Output string of tiles to screen
-	OP(FLS);	// Fill screen with same tile
-	OP(PAL);	// Set palette color
-	OP(CHR);	// Set charset data
-	OP(LDCHR);	// Load charset data from image file
-	OP(LDPAL);	// Load palette data from image file
-	OP(PRTSCN);	// Save screenshot
-	OP(COLS);	// Get screen column count
-	OP(ROWS);	// Get screen row count
+	Op["FWND"] = &FWND;			// Enable/disable fullscreen mode
+	Op["UPDS"] = &UPDS;			// Enable/disable automatic screen update
+	Op["CLS"] = &CLS;			// Clear screen
+	Op["OUTM"] = &OUTM;			// Select output mode
+	Op["OUT"] = &OUT;			// Output tile to screen
+	Op["OUTS"] = &OUTS;			// Output string of tiles to screen
+	Op["FLS"] = &FLS;			// Fill screen with same tile
+	Op["PAL"] = &PAL;			// Set palette color
+	Op["CHR"] = &CHR;			// Set charset data
+	Op["LDCHR"] = &LDCHR;		// Load charset data from image file
+	Op["LDPAL"] = &LDPAL;		// Load palette data from image file
+	Op["PRTSCN"] = &PRTSCN;		// Save screenshot
+	Op["COLS"] = &COLS;			// Get screen column count
+	Op["ROWS"] = &ROWS;			// Get screen row count
 
 	//=== INPUT ===
-	OP(IN);		// Get key pressed
+	Op["IN"] = &IN;				// Get key pressed
 
 	//=== SOUND ===
-	OP(SSND);	// Stop all currently playing sounds
-	OP(SWFT);	// Set waveform type
-	OP(VOL);	// Set sound volume
-	OP(BEEP);	// Play a single beep
-	OP(PLAY);	// Play notes from a sound string (once)
-	OP(LPLAY);	// Play notes from a sound string (loop)
+	Op["SSND"] = &SSND;			// Stop all currently playing sounds
+	Op["SWFT"] = &SWFT;			// Set waveform type
+	Op["VOL"] = &VOL;			// Set sound volume
+	Op["BEEP"] = &BEEP;			// Play a single beep
+	Op["PLAY"] = &PLAY;			// Play notes from a sound string (once)
+	Op["LPLAY"] = &LPLAY;		// Play notes from a sound string (loop)
 
 	//=== FILESYSTEM ===
-	OP(READ);	// Read file into string
-	OP(WRITE);	// Write string to file
-	OP(DEL);	// Delete file
+	Op["FREAD"] = &FREAD;		// Read file into string
+	Op["FWRITE"] = &FWRITE;		// Write string to file
+	Op["FDEL"] = &FDEL;			// Delete file
 }
